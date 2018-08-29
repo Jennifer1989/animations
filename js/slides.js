2ee2adb895b45344e4e89fd649bb2a98daccc84f
双的肾 <jennifer-liu@outlook.com>
2018-08-29 22:13:49 +0800

    initialize

---
+++ js/slides.js
@@ -0,0 +1,7 @@
+let buttons = document.querySelector('slide-buttons'),
+    slides = document.getElementById('slides');
+buttons.addEventListener('click', function(event) {
+
+    var data_id = event.target['data-id'];
+
+})
