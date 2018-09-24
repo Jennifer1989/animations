18828acf91a2da8665aad049a5a1e4e92f6a9cd9
双的肾 <jennifer-liu@outlook.com>
2018-09-24 10:56:41 +0800

    add slides rolling play with prev and next button

--- js/slides.js
+++ js/slides.js
@@ -1,7 +1,10 @@
 let buttons = document.querySelector('slide-buttons'),
-    slides = document.getElementById('slides');
+    slides = document.getElementById('slides'),
+    move = function(num) {
+    	
+    };
 buttons.addEventListener('click', function(event) {
 
     var data_id = event.target['data-id'];
-
+    move(data_id);
 })
